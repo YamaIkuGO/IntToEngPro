5c853f74dc678b683a309b698123eae563652b5d
g16950yi <g16950yi@s30422.tsuda.ac.jp>
2018-05-17 14:50:55 +0900

    ちょい変更

--- test/IntToEng/IntToEngTest.java
+++ test/IntToEng/IntToEngTest.java
@@ -1,11 +1,53 @@
 package IntToEng;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class IntToEngTest {
 
+	@Test
+	public void てすと0() {
+		IntToEng num = new IntToEng();
+		String expected ="zero";
+		String actual = num.translateEng(0);
+		assertThat(actual,is(expected));
+	} 
+	
+	@Test
+	public void てすと1() {
+		IntToEng num = new IntToEng();
+		String expected ="one";
+		String actual = num.translateEng(1);
+		assertThat(actual,is(expected));
+	} 
+	
+	@Test
+	public void てすと11() {
+		IntToEng num = new IntToEng();
+		String expected ="eleven";
+		String actual = num.translateEng(11);
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void てすと17() {
+		IntToEng num = new IntToEng();
+		String expected ="seventeen";
+		String actual = num.translateEng(17);
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void てすと21() {
+		IntToEng num = new IntToEng();
+		String expected ="twenty one";
+		String actual = num.translateEng(21);
+		assertThat(actual,is(expected));
+	} 
+
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
